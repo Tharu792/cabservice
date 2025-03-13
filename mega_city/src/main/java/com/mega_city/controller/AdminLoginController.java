5ddfa2884b734498b4096994033878705f7a03eb
DESKTOP-220BTBJ\Giga Hertz <tharushigamaarachchi@gmail.com>
2025-03-13 19:41:33 +0530

    fix warning

--- mega_city/src/main/java/com/mega_city/controller/AdminLoginController.java
+++ mega_city/src/main/java/com/mega_city/controller/AdminLoginController.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 
 @WebServlet("/AdminLoginController")
 public class AdminLoginController extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
 
     private AdminLoginService adminLoginService = new AdminLoginService();
 
